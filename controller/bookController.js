4bb716b5f06e2aa193764b773f363c4c0f198a08
xanhtran <xanhtran.it04@gmail.com>
2021-12-29 14:30:24 +0700

    fix: change bookController file

--- controller/bookController.js
+++ controller/bookController.js
@@ -7,7 +7,6 @@ var BookInstance = require('../models/bookinstance');
 var async = require('async');
 
 exports.index = function(req, res) {
-
     async.parallel({
         book_count: function(callback) {
             Book.countDocuments({}, callback); // Pass an empty object as match condition to find all documents of this collection
@@ -27,6 +26,7 @@ exports.index = function(req, res) {
     }, function(err, results) {
         res.render('index', { title: 'Local Library Home', error: err, data: results });
     });
+   
 };
 
 // Display list of all books.
